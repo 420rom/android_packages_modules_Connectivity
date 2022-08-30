0aa11180f2e9f059f4224f3984cfe28aac5bec2c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-08-30 13:18:56 +0300

    use Cloudflare DNS for MTU detection in CLAT initialization

--- service/src/com/android/server/connectivity/ClatCoordinator.java
+++ service/src/com/android/server/connectivity/ClatCoordinator.java
@@ -79,7 +79,7 @@ public class ClatCoordinator {
     static final String INIT_V4ADDR_STRING = "192.0.0.4";
     @VisibleForTesting
     static final int INIT_V4ADDR_PREFIX_LEN = 29;
-    private static final InetAddress GOOGLE_DNS_4 = InetAddress.parseNumericAddress("8.8.8.8");
+    private static final InetAddress CLOUDFLARE_DNS_4 = InetAddress.parseNumericAddress("1.1.1.1");
 
     private static final int INVALID_IFINDEX = 0;
 
@@ -570,8 +570,14 @@ public class ClatCoordinator {
         }
 
         // Detect ipv4 mtu.
-        final int detectedMtu = mDeps.detectMtu(pfx96Str,
-                ByteBuffer.wrap(GOOGLE_DNS_4.getAddress()).getInt(), fwmark);
+        final int detectedMtu;
+        try {
+            detectedMtu = mDeps.detectMtu(pfx96Str,
+                ByteBuffer.wrap(CLOUDFLARE_DNS_4.getAddress()).getInt(), fwmark);
+        } catch (IOException e) {
+            maybeCleanUp(tunFd, readSock6, writeSock6);
+            throw new IOException("Detect MTU on " + tunIface + " failed: " + e);
+        }
         final int mtu = adjustMtu(detectedMtu);
         Log.i(TAG, "ipv4 mtu is " + mtu);
 
